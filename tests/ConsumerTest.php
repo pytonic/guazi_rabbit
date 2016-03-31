f712e7047f1c862230a51fcea99838c307b18724
ZhaiXiaolei <zhaixiaolei@guazi.com>
2016-03-31 17:08:59 +0800

    修改消费端测试代码，现在无法做mock，之后再改。

--- tests/ConsumerTest.php
+++ tests/ConsumerTest.php
@@ -10,27 +10,14 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 class ConsumerTest extends TestCase{
 
     private $config;
-    /**
-     * @var \Gua\Consumer
-     */
-    private $client;
 
     public function setUp() {
         $this->config = include dirname(__DIR__).'/config/rabbit.php';
-        $config = $this->config;
-        $class        = $this->getMockClass(\Gua\Consumer::class);
-        $connection = new AMQPStreamConnection(
-            array_get($config, 'connection.host'),
-            array_get($config, 'connection.port'),
-            array_get($config, 'connection.user'),
-            array_get($config, 'connection.password'),
-            array_get($config, 'connection.vhost', '/')
-        );
-        $this->client = new $class($connection,$this->config);
     }
 
     public function testClient() {
-        $this->client->queue('console')->consume('payments',function($message){
+        $consumer = \Gua\Consumer::with($this->config);
+        $consumer->queue('console')->consume('payments',function($message){
             echo $message;
         });
     }
